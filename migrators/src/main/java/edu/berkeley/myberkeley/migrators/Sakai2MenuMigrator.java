f72edc0c9483f506a641da152e90c39989b8f891
John King <johnk@media.berkeley.edu>
2012-05-03 10:49:36 -0700

    MYB-1176 - first version where migrator is invoked, still has infinite JSONObject loop

--- migrators/src/main/java/edu/berkeley/myberkeley/migrators/Sakai2MenuMigrator.java
+++ migrators/src/main/java/edu/berkeley/myberkeley/migrators/Sakai2MenuMigrator.java
@@ -21,10 +21,17 @@ import static org.sakaiproject.nakamura.lite.content.InternalContent.PATH_FIELD;
 import static org.sakaiproject.nakamura.lite.content.InternalContent.STRUCTURE_UUID_FIELD;
 
 import com.google.common.collect.ImmutableMap;
+
+import java.util.Iterator;
 import java.util.Map;
+
+import javax.servlet.Servlet;
+
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
+import org.apache.sling.commons.json.JSONException;
+import org.apache.sling.commons.json.JSONObject;
 import org.sakaiproject.nakamura.api.lite.ClientPoolException;
 import org.sakaiproject.nakamura.api.lite.PropertyMigrator;
 import org.sakaiproject.nakamura.api.lite.Repository;
@@ -35,7 +42,7 @@ import org.sakaiproject.nakamura.api.lite.content.Content;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Service
+@Service({PropertyMigrator.class})
 @Component
 public class Sakai2MenuMigrator implements PropertyMigrator {
   private static final Logger LOGGER = LoggerFactory.getLogger(Sakai2MenuMigrator.class);
@@ -45,48 +52,134 @@ public class Sakai2MenuMigrator implements PropertyMigrator {
 
   @Override
   public boolean migrate(String rowId, Map<String, Object> properties) {
-    if (properties.containsKey("sling.resourceType")) {
-      String resourceType = properties.get("sling.resourceType").toString(); 
+    if (properties.containsKey("sling:resourceType")) {
+      String resourceType = properties.get("sling:resourceType").toString(); 
       if (resourceType.equals("sakai/private")) {
         migratePrivSpace(properties);
       } else if (resourceType.equals("sakai/public")) {
         migragePubSpace(properties);
       }
     }
-    if (properties.containsKey(PATH_FIELD) && !properties.containsKey(STRUCTURE_UUID_FIELD)) {
-      String path = properties.get(PATH_FIELD).toString();
-      Session session = null;
-      try {
-        session = repository.loginAdministrative();
-        Content content = session.getContentManager().get(path);
-        if (content == null) {
-          LOGGER.warn("Null content for {}", path);
-        }
-      } catch (AccessDeniedException e) {
-        LOGGER.error(e.getMessage(), e);
-      } catch (ClientPoolException e) {
-        LOGGER.error(e.getMessage(), e);
-      } catch (StorageClientException e) {
-        LOGGER.error(e.getMessage(), e);
-      } finally {
-        if (session != null) {
-          try {
-            session.logout();
-          } catch (ClientPoolException e) {
-            LOGGER.error("Unexpected exception logging out of session", e);
-          }
-        }
-      }
-    }
+    
     return false;
   }
 
-  private void migragePubSpace(Map<String, Object> properties) {
+  private void migratePrivSpace(Map<String, Object> properties) {
+    Session session = null;
+    try {
+      if (properties.containsKey(PATH_FIELD)) {
+        session = repository.loginAdministrative();
+        String path = properties.get(PATH_FIELD).toString();
+        Content privSpaceContent = session.getContentManager().get(path);
+        Iterable<Content> children = privSpaceContent.listChildren();
+        Iterator<Content> contentIter = children.iterator();
+        while (contentIter.hasNext()) {
+          Content privspace = contentIter.next();
+          if (privspace.getPath().contains("privspace")) {
+            migrateStruc0(privspace);
+            migrateDashboard(privspace);
+            migrateRows(privspace);
+          }
+        }
+      }
+    } catch (ClientPoolException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (StorageClientException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (AccessDeniedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (JSONException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } finally {
+      if (session != null) {
+        try {
+          session.logout();
+        } catch (ClientPoolException e) {
+          LOGGER.error("Unexpected exception logging out of session", e);
+        }
+      }
+    }
+    
+  }
+  
+  private void migrateStruc0(Content privspace) throws JSONException {
+    JSONObject struc0JSON = findStructure0(privspace);
+    JSONObject menuObj = new JSONObject();
+    menuObj.put("_ref", "${refid}2345");
+    menuObj.put("_title", "My bSpace sites");
+    menuObj.put("_order", 2);
+    menuObj.put("_canEdit", Boolean.TRUE);
+    menuObj.put("_reorderOnly", Boolean.TRUE);
+    menuObj.put("_nonEditable", Boolean.TRUE);
+    JSONObject mainObj = new JSONObject();
+    mainObj.put("_ref", "${refid}2345");
+    mainObj.put("_order", 0);
+    mainObj.put("_title", "My Sakai 2 sites");
+    menuObj.put("main", menuObj);
+    struc0JSON.put("sakai2sites", menuObj.toString());
+  //config.defaultprivstructure.structure0['sakai2sites'] =  {
+//  '_ref': '${refid}2345',
+//  '': 'My bSpace sites',
+//  '_order': 2,
+//  '_canEdit': true,
+//  '_reorderOnly': true,
+//  '_nonEditable': true,
+//  'main': {
+//      '_ref': '${refid}2345',
+//      '_order': 0,
+//      '_title': 'My Sakai 2 sites'
+//  }
+//};
   }
 
-  private void migratePrivSpace(Map<String, Object> properties) {
-    // TODO Auto-generated method stub
+  private void migrateRows(Content privspace) {
     
+//    config.defaultprivstructure['${refid}2345'] = {
+//        'rows': [
+//            {
+//                'id': 'id8965114',
+//                'columns': [
+//                    {
+//                        'width': 1,
+//                        'elements': [
+//                            {
+//                                'id': '${refid}2346',
+//                                'type': 'searchsakai2'
+//                            }
+//                        ]
+//                    }
+//                ]
+//            }
+//        ]
+//    };
+  }
+
+  private void migrateDashboard(Content privspace) {
+    
+    
+//    config.defaultprivstructure['${refid}0']['${refid}5'].dashboard.columns.column1.push({
+//      'uid': '${refid}1234',
+//      'visible': 'block',
+//      'name': 'mysakai2'
+//  });
+  }
+
+  
+  private JSONObject findStructure0(Content foundContent) throws JSONException {
+    JSONObject structure0 = null;
+    Object structureObj = foundContent.getProperty("structure0");
+    if (structureObj != null) {
+      structure0 = new JSONObject(structureObj.toString());
+    }
+    return structure0;
+  }                     
+
+  private void migragePubSpace(Map<String, Object> properties) {
+
   }
 
   @Override
@@ -96,7 +189,7 @@ public class Sakai2MenuMigrator implements PropertyMigrator {
 
   @Override
   public String getName() {
-    return MissingContentMigrator.class.getName();
+    return Sakai2MenuMigrator.class.getName();
   }
 
   @Override
