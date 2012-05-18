2f8919ae52f06d096ad614062a41f682eb1c7894
John King <johnk@media.berkeley.edu>
2012-05-18 11:43:46 -0700

    MYB-1622 - more name refactoring to reflect the fact that we are provisioning class rather than course data

--- provision/src/main/java/edu/berkeley/myberkeley/provision/render/AbstractClassPageRenderer.java
+++ provision/src/main/java/edu/berkeley/myberkeley/provision/render/AbstractClassPageRenderer.java
@@ -4,14 +4,14 @@ package edu.berkeley.myberkeley.provision.render;
 import org.sakaiproject.nakamura.api.lite.Repository;
 
 
-abstract class AbstractCourseInfoRenderer implements CourseInfoRenderer{
+abstract class AbstractClassPageRenderer implements ClassPageRenderer{
   
   protected Repository respository;
   
   // to allow passing in of template from a file later
   protected String template;
   
-  protected AbstractCourseInfoRenderer(Repository repository, String template) {
+  protected AbstractClassPageRenderer(Repository repository, String template) {
     this.respository = repository;
     this.template = template;
   }
