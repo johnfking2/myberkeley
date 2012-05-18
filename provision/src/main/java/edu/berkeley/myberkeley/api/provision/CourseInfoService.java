324e1280e678d5d0cc308b911b5784657fecc352
John King <johnk@media.berkeley.edu>
2012-05-18 10:12:57 -0700

    MYB-1622 - refactoring courseId to classId to avoid confusion

--- provision/src/main/java/edu/berkeley/myberkeley/api/provision/CourseInfoService.java
+++ provision/src/main/java/edu/berkeley/myberkeley/api/provision/CourseInfoService.java
@@ -3,7 +3,9 @@ package edu.berkeley.myberkeley.api.provision;
 import org.apache.sling.commons.json.JSONObject;
 
 public interface CourseInfoService {
-  public JSONObject getCourseInfo(String courseId);
+  public JSONObject getCourseInfo(String classId);
   
-  public CourseInfoProvisionResult saveCourseInfo(JSONObject courseInfo);
+  public CourseInfoProvisionResult provisionCourseInfo(JSONObject classPage);
+  
+  public CourseInfoProvisionResult provisionCourseInfo(String classId);
 }
--- provision/src/main/java/edu/berkeley/myberkeley/provision/CalCourseInfoService.java
+++ provision/src/main/java/edu/berkeley/myberkeley/provision/CalCourseInfoService.java
@@ -2,14 +2,20 @@ package edu.berkeley.myberkeley.provision;
 
 import edu.berkeley.myberkeley.api.provision.CourseInfoProvisionResult;
 import edu.berkeley.myberkeley.api.provision.CourseInfoService;
-import edu.berkeley.myberkeley.api.provision.ProvisionResult;
 import edu.berkeley.myberkeley.api.provision.SynchronizationState;
+import edu.berkeley.myberkeley.provision.provide.CourseAttributeProvider;
+import edu.berkeley.myberkeley.provision.provide.OracleCourseHeaderAttributeProvider;
+import edu.berkeley.myberkeley.provision.render.CourseInfoHeaderRenderer;
+import edu.berkeley.myberkeley.provision.render.CourseInfoRenderer;
 
+import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Modified;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.commons.json.JSONException;
 import org.apache.sling.commons.json.JSONObject;
+import org.osgi.service.component.ComponentContext;
 import org.sakaiproject.nakamura.api.lite.ClientPoolException;
 import org.sakaiproject.nakamura.api.lite.Repository;
 import org.sakaiproject.nakamura.api.lite.Session;
@@ -20,7 +26,9 @@ import org.sakaiproject.nakamura.api.lite.content.ContentManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Dictionary;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component(metatype = true,
@@ -28,34 +36,72 @@ import java.util.Map;
 @Service
 public class CalCourseInfoService implements CourseInfoService {
 
-  public static final String STORE_NAME = "_myberkeley_courseInfo";
-  public static final String STORE_RESOURCETYPE = "myberkeley/courseinfo";
+  public static final String STORE_NAME = "_myberkeley_classpage";
+  public static final String STORE_RESOURCETYPE = "myberkeley/c";
+  public static final String CLASS_PAGE_PROP_NAME = "classPzge";
   
   private static final Logger LOGGER = LoggerFactory.getLogger(CalCourseInfoService.class);
+  
+  private Map<Component, CourseInfoRenderer> renderers;
+  
+  private Map<Component, CourseAttributeProvider> attributeProviders;
+  
+  private enum Component {
+    classPageHeader
+  }
   
   @Reference
   Repository repository;
   
   @Override
-  public JSONObject getCourseInfo(String courseId) {
-    return null;
+  public JSONObject getCourseInfo(String classId) {
+    JSONObject courseInfo = null;
+    Session adminSession = null;
+    try {
+      adminSession = repository.login();
+      ContentManager cm = adminSession.getContentManager();
+      Content courseInfoContent = cm.get(STORE_NAME + "/" + classId);
+      if (courseInfoContent != null) {
+        String courseInfoStr = (String) courseInfoContent.getProperty(CLASS_PAGE_PROP_NAME);
+        courseInfo = new JSONObject(courseInfoStr);
+      } else {
+        LOGGER.warn("No classPageInkfo found for classId: " + classId);
+      }
+    } catch (ClientPoolException e) {
+      LOGGER.warn(e.getMessage(), e);
+    } catch (StorageClientException e) {
+      LOGGER.warn(e.getMessage(), e);
+    } catch (AccessDeniedException e) {
+      LOGGER.warn(e.getMessage(), e);
+    } catch (JSONException e) {
+      LOGGER.warn(e.getMessage(), e);
+    } finally {
+      if (adminSession != null) {
+        try {
+          adminSession.logout();
+        } catch (ClientPoolException e) {
+          LOGGER.warn(e.getMessage(), e);
+        }
+      }
+    }
+    return courseInfo;
   }
 
   @Override
-  public CourseInfoProvisionResult saveCourseInfo(JSONObject courseInfo) {
+  public CourseInfoProvisionResult provisionCourseInfo(JSONObject courseInfo) {
     Session adminSession = null;
     SynchronizationState synchronizationState = SynchronizationState.error;
     Content courseInfoContent = null;
-    String courseId = null;
+    String classId = null;
     Map<String, Object> courseInfoMap = null;
     try {
       adminSession = repository.loginAdministrative();
       ContentManager cm = adminSession.getContentManager();
-      courseId = courseInfo.getString("courseid");
-      if (courseId == null) {
-        throw new JSONException("courseId key missing");
+      classId = courseInfo.getString("classid");
+      if (classId == null) {
+        throw new JSONException("classid key missing");
       }
-      String path = STORE_NAME + "/" + courseId;
+      String path = STORE_NAME + "/" + classId;
       String stringifiedClaseeInfo = courseInfo.toString();
       courseInfoContent = cm.get(path);
       if (courseInfoContent == null) {
@@ -89,7 +135,72 @@ public class CalCourseInfoService implements CourseInfoService {
         }
       }
     }
-    return new CourseInfoProvisionResult(courseId, synchronizationState);
+    return new CourseInfoProvisionResult(classId, synchronizationState);
+  }
+
+  @Override
+  public CourseInfoProvisionResult provisionCourseInfo(String classId) {
+    CourseInfoProvisionResult result = null;
+    Content courseInfoContent = null;
+    Map<String, Object> courseInfoMap = null;
+    Session adminSession = null;
+    SynchronizationState synchronizationState = SynchronizationState.error;
+    try {
+      adminSession = repository.loginAdministrative();
+      ContentManager cm = adminSession.getContentManager();
+      String path = STORE_NAME + "/" + classId;
+      courseInfoContent = cm.get(path);
+      if (courseInfoContent == null) {
+        courseInfoMap = new HashMap<String, Object>(2);
+        courseInfoMap.put("sling:resourceType", STORE_RESOURCETYPE);
+//        courseInfoMap.put("courseInfo", stringifiedClaseeInfo);
+        JSONObject courseInfoJSON = buildJSON(classId);
+      }
+    } catch (ClientPoolException e) {
+      LOGGER.warn(e.getMessage(), e);
+    } catch (StorageClientException e) {
+      LOGGER.warn(e.getMessage(), e);
+    } catch (AccessDeniedException e) {
+      LOGGER.warn(e.getMessage(), e);
+    } finally {
+      if (adminSession != null) {
+        try {
+          adminSession.logout();
+        } catch (ClientPoolException e) {
+          LOGGER.warn(e.getMessage(), e);
+        }
+      }      
+    }
+    
+    return result;
+  }
+  
+  private JSONObject buildJSON(String classId) {
+    JSONObject courseInfo = null;
+    CourseAttributeProvider headerProvider = this.attributeProviders.get(Component.classPageHeader);
+    List<Map<String, Object>> headerAttriubesList = headerProvider.getAttributes(classId);
+    CourseInfoRenderer headerRenderer = this.renderers.get(Component.classPageHeader);
+    try {
+      JSONObject headerJSON = headerRenderer.render(headerAttriubesList.get(0));
+      LOGGER.debug("courseInfoHeader:" + headerJSON.toString());
+    } catch (JSONException e) {
+      LOGGER.warn(e.getMessage(), e);
+    }
+    return courseInfo;
+  }
+
+  @Activate @Modified
+  protected void activate(ComponentContext componentContext) {
+    // wire up later to allow pipeline configurataion and template passing??
+    Dictionary<?, ?> props = componentContext.getProperties();
+    
+    // cab't get an ImmutableMap to handle types so using plain HashMaqp    
+    this.attributeProviders = new HashMap<CalCourseInfoService.Component, CourseAttributeProvider>();
+    this.attributeProviders.put(Component.classPageHeader, new OracleCourseHeaderAttributeProvider());
+
+    this.renderers = new HashMap<Component, CourseInfoRenderer>();
+    this.renderers.put(Component.classPageHeader, new CourseInfoHeaderRenderer(repository, null));
+      
   }
 
 }
