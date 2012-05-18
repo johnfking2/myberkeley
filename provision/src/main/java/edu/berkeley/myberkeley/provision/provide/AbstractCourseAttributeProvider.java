5ac8a8b7e233ed688e6233e7d5fe6bcae04b0caa
John King <johnk@media.berkeley.edu>
2012-05-17 17:58:57 -0700

    MYB-1622 - bringing up separate attribute providers and rederers framework

---
+++ provision/src/main/java/edu/berkeley/myberkeley/provision/provide/AbstractCourseAttributeProvider.java
@@ -0,0 +1,18 @@
+package edu.berkeley.myberkeley.provision.provide;
+
+import edu.berkeley.myberkeley.api.provision.JdbcConnectionService;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
+
+import java.util.List;
+import java.util.Map;
+
+public abstract class AbstractCourseAttributeProvider implements CourseAttributeProvider {
+
+
+  @Reference
+ protected JdbcConnectionService jdbcConnectionService;
+
+  public abstract List<Map<String, Object>> getAttributes(String courseId);
+}
